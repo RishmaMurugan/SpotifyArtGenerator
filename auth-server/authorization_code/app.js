cdfb9794b76b0057d80bba2e838be6b79b5b0e21
Rishma Murugan <rishma2000@gmail.com>
2020-08-14 16:56:40 -0700

    Adding client code

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '406b54622b51482c88fdda1aa8754471'; // Your client id
+var client_secret = 'af9c4ac79e114b30aebca3f0d97450d0'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
