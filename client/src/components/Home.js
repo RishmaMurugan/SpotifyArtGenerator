18e24409f2943d34fc59b54775188b16d0f1917c
Rishma Murugan <rishma2000@gmail.com>
2020-08-15 14:49:36 -0700

    Working navbar

---
+++ client/src/components/Home.js
@@ -0,0 +1,50 @@
+import React, {Component} from './node_modules/react';
+import './App.css';
+import SpotifyWebApi from './node_modules/spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
+
+class Home extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+  getNowPlaying(){
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: { 
+              name: response.item.name, 
+              albumArt: response.item.album.images[0].url
+            }
+        });
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+          <h1>Test</h1>
+    </div>
+    );
+  }
+  
+}
+
+export default Home;
---
+++ client/src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'FAQs',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
